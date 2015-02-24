8fd2742677ab153757fe64bdcf5123c11213f4cd
Pinchacarballos <oskar4416@gmail.com>
2015-02-24 09:45:04 +0100

    Cafrada Oskar v3

--- Formulario1/Formulario1/Form1.Designer.vb
+++ Formulario1/Formulario1/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,11 +36,22 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Prueba syncro"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 23.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(209, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(277, 35)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cafrada Oskiiña v3"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(756, 374)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -48,5 +60,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- Formulario1/Formulario1/Form1.vb
+++ Formulario1/Formulario1/Form1.vb
@@ -7,4 +7,8 @@
     Private Sub Label2_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub Label2_Click_1(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
 End Class

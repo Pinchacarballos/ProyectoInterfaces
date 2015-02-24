1e872302e6767b4f70254b6084e7bc318ffa9a2e
Pinchacarballos <oskar4416@gmail.com>
2015-02-24 10:03:29 +0100

    Creadas Bibliotecas de clases

--- Formulario1/Formulario1/Form1.Designer.vb
+++ Formulario1/Formulario1/Form1.Designer.vb
@@ -39,10 +39,9 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 23.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(209, 157)
+        Me.Label2.Location = New System.Drawing.Point(369, 143)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(277, 35)
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Cafrada Oskiiña v3"
         '
